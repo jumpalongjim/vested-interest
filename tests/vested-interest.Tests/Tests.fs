019fb93bea6083d7a9bbb03256b50591a23330cc
James <james.johnson@infuse.it>
2015-06-04 17:30:59 +0100

    Add ProjectScaffold

---
+++ tests/vested-interest.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module vested-interest.Tests
+
+open vested-interest
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
